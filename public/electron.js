b0b7a68e47dc84cd8e2fbe5679ced7a6230ef0b2
wisemk79 <wisemk79@gmail.com>
2020-10-21 18:19:11 +0900

    update

--- public/electron.js
+++ public/electron.js
@@ -15,13 +15,10 @@ function createWindow () {
   // 3. and load the index.html of the app.
   if (isDev) {
     // 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드
-    // mainWindow.loadURL('http://localhost:3000');
-    console.log('production => ', path.join(__dirname, '../build/index.html'))
-    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+    mainWindow.loadURL('http://localhost:3000');
     mainWindow.webContents.openDevTools();
   } else {
     // 프로덕션 환경에서는 패키지 내부 리소스에 접근
-    console.log('production => ', path.join(__dirname, '../build/index.html'))
     mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
   }
   
